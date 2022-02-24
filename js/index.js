6d3b944a21275858120c6c1c92907b9a6e5f5437
Emmanuel Correa <ebcorreac@gmail.com>
2022-02-24 20:07:27 -0300

    cambio de span a abajo

--- js/index.js
+++ js/index.js
@@ -1,5 +1,8 @@
 const name = 'EMMANUEL CORREA';
 const profession = ['FULL-STACK DEVELOPER','WEB DEVELOPER', 'FLUTTER DEVELOPER', 'PODCASTER'];
+const lng = [{libs:'Javascripts', age: 2}, {libs:'TypeScript', age: 2}, {libs:'PHP', age: 2 }, {libs:'C#', age: 1 }, {libs:'Dart', age: 2}];
+const tecnologies = [{libs:'React', age: 1}, {libs:'Flutter', age: 2}, {libs:'Codeigneiter', age: 2}, {libs:'Node', age: 1 }, {libs:'Express', age: 1}];
+const portfolio = [{title:'INVAPP', description:'App de inventario para iglesia comisionado sin fronteras, santiago-Chile', type:'full' ,url:'iglesiacsf.cl/invapp/', img:''}, {title:'TRANSMITO.CL', description:'', url:'transmito.cl/', img:'', type: 'web'}, {title:'OPTICA - MONTECARLOS', description:'Ecommerce realizado con WordPress para optica de la ciudad de Los Lagos - Chile', url:'opticamontecarlos.cl/', img:'', type:'webs'}];
 const age = '27';
 const address = 'Calle San Juan, San Juan, San Juan, Puerto Rico';
 const city = 'Los Lagos';
