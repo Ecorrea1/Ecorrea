7fff417a014ed58b839983c700ec952e43756b81
Emmanuel Correa <ebcorreac@gmail.com>
2022-02-26 02:53:44 -0300

    fix de politicas de privacidad

--- js/templates/footer.js
+++ js/templates/footer.js
@@ -10,7 +10,7 @@ class Footer extends HTMLElement {
         <div class="footer-copyright text-center">© 2022 -
           <a class="text-center links-2" href="http://ecorrea.cl/">ECORREA by EMMANUEL CORREA</a>
           CONTRUYENDO APLICACIONES CON PASION DESDE LOS LAGOS PARA EL MUNDO | 
-          <a class="text-center links-2" href="http://127.0.0.1:5500/politica-de-privacidad.html"> POLITICAS DE PRIVACIDAD </a> 
+          <a class="text-center links-2" href="http://ecorrea.cl/politica-de-privacidad.html"> POLITICAS DE PRIVACIDAD </a> 
         </div>
         <div class="footer-logo">
           <a class="links-2" href="http://ecorrea.cl/"> ECORREA </a>
