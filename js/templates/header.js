ea7c4b89398dcbccf49464bd89ef9146ca1a8864
Emmanuel Correa <ebcorreac@gmail.com>
2022-03-01 23:05:43 -0300

    cambio de navbar

--- js/templates/header.js
+++ js/templates/header.js
@@ -5,11 +5,9 @@ class Header extends HTMLElement {
 
     connectedCallback() {
         this.innerHTML = `
-        <header class="header">
-
         <nav id="navBar" class="navbar navbar-expand-lg navbar-dark">
           <div class="container-fluid">
-            <a class="navbar-brand" href="#"><img src="/assets/img/logo.png" alt="ecorrea-logo" width="24" height="24" class="d-inline-block align-text-top img-logo"> 
+            <a class="navbar-brand" href=""><img src="/assets/img/logo.png" alt="ecorrea-logo" width="24" height="24" class="d-inline-block align-text-top img-logo"> 
               Emmanuel Correa
             </a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -33,25 +31,6 @@ class Header extends HTMLElement {
             </div>
           </div>
         </nav>
-  
-        <div class="title-header">
-          <div class="container-hello">
-            <h1><span>HOLA,</span></h1> 
-          </div>
-          <div id="description-short" class="description-short">
-            <h2 class="text-center">
-              MI NOMBRE ES <span id="name" class="name"></span>,
-            </h2>
-            <h2 class="text-center">
-              <span id="profession" class="profession" style="color:#44CCD3"></span>
-            </h2>
-          </div> 
-          <div id="container-btn-down" class="container-fluid">
-           <a href="#hello" id="btn-move-down" class="link-light inner-link">V</a> 
-           <!-- <a id="btn-move-down" class="link-light inner-link"> V </a> -->
-          </div>
-        </div>  
-      </header>
         `;
     }
 
